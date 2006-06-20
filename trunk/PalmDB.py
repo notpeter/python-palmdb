1355a646d9a100a0c245be6b5b9d5aa1a61ef822
rick_price <rick_price@6617d062-5610-0410-b5ef-fbf74ddf481c>
2006-06-20 22:38:33 +0000

    Change license to LGPL because old license did not make sense.
    I would like anyone who would like to use the library to be able to use it, so if you need a different license, feel free to contact me.

--- trunk/PalmDB.py
+++ trunk/PalmDB.py
@@ -1,12 +1,25 @@
 #
 #  $Id: PalmDB.py,v 1.11 2005/12/13 03:12:12 rprice Exp $
 #
-#  Copyright 2004 Rick price <rick_price@users.sourceforge.net>
+#  Copyright 2006 Rick price <rick_price@users.sourceforge.net>
+#  This Python library is used to read/write Palm PDB files
 #
-#  This file may be used in accordance with the Python Software License 2.3.4 or any later version.
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
 #
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
 #  This code was based on code written by Rob Tillotson <rob@pyrite.org>, but has been heavily
-#  modified.
+#  modified so that it is now basically code I have written.
 
 
 """PRC/PDB file I/O in pure Python.
@@ -19,7 +32,7 @@
 
 __version__ = '$Id: PalmDB.py,v 1.11 2005/12/13 03:12:12 rprice Exp $'
 
-__copyright__ = 'Copyright 2004 Rick Price <rick_price@users.sourceforge.net>'
+__copyright__ = 'Copyright 2006 Rick Price <rick_price@users.sourceforge.net>'
 
 #
 # DBInfo structure:
