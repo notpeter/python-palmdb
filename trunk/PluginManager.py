204e50c2ef0502f4112e76ba6e17a3f5a24c3e86
rick_price <rick_price@6617d062-5610-0410-b5ef-fbf74ddf481c>
2006-06-22 18:51:39 +0000

    This is still completely broken.

---
+++ trunk/PluginManager.py
@@ -0,0 +1,60 @@
+#
+#  $Id: PalmDB.py,v 1.11 2005/12/13 03:12:12 rprice Exp $
+#
+# Copyright 2006 Rick price <rick_price@users.sourceforge.net>
+# This Python library is used to read/write Palm PDB files
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+#  This code was based on code written by Rob Tillotson <rob@pyrite.org>, but has been heavily
+#  modified so that it is now basically code I have written.
+
+# This file includes changes made (including better naming, comments and design changes) by Mark Edgington, many thanks Mark.
+
+
+"""PRC/PDB file I/O in pure Python.
+
+"""
+
+__version__ = '$Id: PalmDB.py,v 1.11 2005/12/13 03:12:12 rprice Exp $'
+
+__copyright__ = 'Copyright 2006 Rick Price <rick_price@users.sourceforge.net>'
+
+import Plugins.BasePlugin
+
+# +++ READ THIS +++ Plugins need to implement the interface in Plugins.BasePlugin.BasePDBFilePlugin
+PDBPlugins={}
+def registerPDBPlugin(PDBFilePluginClass):
+	type=PDBFilePluginClass.getPDBCreatorID()
+	if type == None:
+		#+++ FIX THIS +++ need to throw a "what are you trying to do exception"
+		pass
+	PDBPlugins[type]=PDBFilePluginClass
+
+def getPDBPlugin(CreatorID):
+	PDBPlugins.get(CreatorID,Plugins.BasePlugin.BasePDBFilePlugin)
+
+#+++ FIX THIS +++ implement
+# if we cannot find an appropriate plugin, default to one that can handle any type
+
+#
+#--------- Register Standard Plugins that come with Library ---------
+#
+
+#import ProgectPDBPlugin
+#import StandardNotepadPDBPlugin
+
+#registerPDBPLugin(ProgectPDBPlugin.plugin)
+#registerPDBPLugin(StandardNotepadPDBPlugin.plugin)
