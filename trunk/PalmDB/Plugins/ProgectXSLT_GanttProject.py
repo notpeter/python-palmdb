e45d3c17b7d66ecfe703e9bd62b6e5a10d829373
rick_price <rick_price@6617d062-5610-0410-b5ef-fbf74ddf481c>
2006-07-18 04:55:40 +0000

    One way translation to GanttProject is now basically working, although much will need to be added. Thankfully GanttProject does not crash if you do not give it attributes...

---
+++ trunk/PalmDB/Plugins/ProgectXSLT_GanttProject.py
@@ -0,0 +1,65 @@
+XSLT_GanttProject_ToDesktop=\
+'''
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
+<xsl:template match="palmDatabase">
+<project name="Untitled Gantt Project" company="" webLink="http://" view-date="2006-07-18" view-index="0" gantt-divider-location="316" resource-divider-location="-1" version="2.0">
+<description></description>
+<tasks color="#8cb6ce">
+<taskproperties>
+<taskproperty id="tpd0" name="type" type="default" valuetype="icon"/>
+<taskproperty id="tpd1" name="priority" type="default" valuetype="icon"/>
+<taskproperty id="tpd2" name="info" type="default" valuetype="icon"/>
+<taskproperty id="tpd3" name="name" type="default" valuetype="text"/>
+<taskproperty id="tpd4" name="begindate" type="default" valuetype="date"/>
+<taskproperty id="tpd5" name="enddate" type="default" valuetype="date"/>
+<taskproperty id="tpd6" name="duration" type="default" valuetype="int"/>
+<taskproperty id="tpd7" name="completion" type="default" valuetype="int"/>
+<taskproperty id="tpd8" name="coordinator" type="default" valuetype="text"/>
+<taskproperty id="tpd9" name="predecessorsr" type="default" valuetype="text"/>
+</taskproperties>
+<xsl:apply-templates select="ProgectDataRecord"/>
+</tasks>
+</project>
+</xsl:template>
+
+<xsl:template match="ProgectDataRecord">
+<task
+id="{recordAttributes/attribute[@name='uid']/*/@value}"
+name="{recordAttributes/attribute[@name='description']/*/@value}"
+expand="{recordAttributes/attribute[@name='opened']/*/@value}"
+>
+<notes><xsl:value-of select="recordAttributes/attribute[@name='note']/*/@value"/></notes>
+<xsl:apply-templates select="children/ProgectDataRecord"/>
+</task>
+</xsl:template>
+
+<!--
+<xsl:template match="attribute">
+<xsl:param name="attributeName" select="@name"/>
+<xsl:attribute name="{$attributeName}">
+<xsl:value-of select="*/@value"/>
+</xsl:attribute>
+</xsl:template>
+-->
+<!--
+<xsl:template match="@*|node()">
+<xsl:copy>
+<xsl:apply-templates select="@*|node()"/>
+</xsl:copy>
+</xsl:template>
+<xsl:template match="@id"/>
+-->
+</xsl:stylesheet>
+'''
+
+XSLT_GanttProject_FromDesktop=\
+'''
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
+<xsl:template match="@*|node()">
+<xsl:copy>
+<xsl:apply-templates select="@*|node()"/>
+</xsl:copy>
+</xsl:template>
+<xsl:template match="@id"/>
+</xsl:stylesheet>
+'''
