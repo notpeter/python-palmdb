da859ebc10eed2795c8a8f99015ad110246e7f44
Rick Price <rprice@pricemail.ca>
2006-10-08 01:25:34 +0000

    Improve the long message text.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-PalmDBVersion = '1.7.1'
+PalmDBVersion = '1.7.2'
 
 setup(
     name="PalmDB",
@@ -9,9 +9,18 @@ setup(
     long_description=
     '''
     This module allows access to Palm OS(tm) database files on the desktop 
-    in pure Python. It is as simple as possible without (hopefully) being 
-    too simple. As much as possible Python idioms have been used to make
-    it easier to use and more versatile.
+    in pure Python.
+    This version is an almost complete rewrite of the original library to
+    use plugins to read/write specific formats. It also uses plugins to
+    read/write XML representations of the PDB. Hooks are provided to use
+    XSLT conversions to go to application specific formats.
+    Really, the only XML conversion that works completely is Palm Progect,
+    but a (untested) sample Todo database plugin has been provided.
+    Work is continuing, and I hope to provide plugins for the standard
+    Palm databases, either written by other people or by myself over time.
+    *Any* database will be converted to XML, but the application specific
+    data will remain opaque, by providing a plugin you can turn the opaque
+    data into something useful.
     ''',
     maintainer="Rick Price",
     maintainer_email="rick_price@users.sourceforge.net",
