e923c17d5b071fe919f521763c48888b7d50c815
Rick Price <rprice@pricemail.ca>
2007-09-18 17:23:09 +0000

    Fix the syntax errors. Fix some latent errors.

--- UnitTests/PluginManagerTest.py
+++ UnitTests/PluginManagerTest.py
@@ -6,6 +6,8 @@ import PalmDB.Plugins.BasePlugin
 class TestPDBPlugin(PalmDB.Plugins.BasePlugin.BasePDBFilePlugin):
 	def getPDBCreatorID(self):
 		return 'TeSt'
+	def getPDBTypeID(self):
+		return 'DaTa'
 
 class PluginManagerTestCase(unittest.TestCase):
 	def tearDown(self):
@@ -14,19 +16,19 @@ class PluginManagerTestCase(unittest.TestCase):
 			deRegisterPDBPlugin(plugin)
 		except:
 			None
- 	def testRegisterNone(self):
- 		'''Attempt to register None as a plugin'''
- 		self.assertRaises(AttributeError,registerPDBPlugin,None)
- 	def testGetPluginNone(self):
- 		'''Attempt to get plugin for a non-existent type'''
+	def testRegisterNone(self):
+		'''Attempt to register None as a plugin'''
+		self.assertRaises(AttributeError,registerPDBPlugin,None)
+	def testGetPluginNone(self):
+		'''Attempt to get plugin for a non-existent type'''
 		self.assertEquals(getPDBPluginByType(None,None),basePlugin)
- 	def testRegisterTestPluginNone(self):
- 		'''Attempt to register test plugin'''
+	def testRegisterTestPluginNone(self):
+		'''Attempt to register test plugin'''
 		plugin=TestPDBPlugin()
 		registerPDBPlugin(plugin)
 		self.assertEquals(getPDBPluginByType(plugin.getPDBCreatorID(),plugin.getPDBTypeID()),plugin)
- 	def testDeRegisterTestPluginNone(self):
- 		'''Attempt to de-register test plugin'''
+	def testDeRegisterTestPluginNone(self):
+		'''Attempt to de-register test plugin'''
 		plugin=TestPDBPlugin()
 		
 		registerPDBPlugin(plugin)
@@ -34,12 +36,7 @@ class PluginManagerTestCase(unittest.TestCase):
 
 		deRegisterPDBPlugin(plugin)
 		# make sure it's gone
- 		self.assertRaises(KeyError,deRegisterPDBPlugin,plugin)
+		self.assertRaises(KeyError,deRegisterPDBPlugin,plugin)
 		
 if __name__ == "__main__":
 	unittest.main()
-
-	def getPDBCreatorID(self):
-#		raise NotImplementedError
-		return 'Unknown'
-	def getPDBTypeID(self):
